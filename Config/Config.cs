37e1b9622c6aacc9664518b26b270cb98af97967
mamba <neelov.ev@mail.ru>
2023-09-25 13:57:22 +0300

    Create project

--- Config/Config.cs
+++ Config/Config.cs
@@ -1,5 +1,38 @@
-﻿namespace Config;
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Config;
 
 public class Config
 {
+    private readonly IConfigurationRoot _configuration;
+
+    public Config()
+    {
+        var configPath = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+        
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile(configPath, optional: true, reloadOnChange: true);
+
+
+        _configuration = builder.Build();
+    }
+
+    public string Get(string key) => _configuration[key];
+
+    public void Set(string key, string value)
+    {
+        var configFile = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+
+        if (!File.Exists(configFile))
+            File.WriteAllText(configFile, "{}");
+
+        var json = File.ReadAllText(configFile);
+        dynamic config = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+        config[key] = value;
+        File.WriteAllText(configFile, Newtonsoft.Json.JsonConvert.SerializeObject(config, Newtonsoft.Json.Formatting.Indented));
+        
+        _configuration.Reload();
+    }
 }
