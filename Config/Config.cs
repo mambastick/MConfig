81450999a41b528af5716bebf7fb5a5c77e3616b
mamba <neelov.ev@mail.ru>
2023-10-02 17:16:15 +0300

    Добавлено шифрование.

--- Config/Config.cs
+++ Config/Config.cs
@@ -1,37 +1,87 @@
 ﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
-namespace Config;
-
-public class AppConfig
+namespace Config
 {
-    private readonly IConfigurationRoot Configuration;
-
-    public AppConfig()
+    public class AppConfig
     {
-        var configPath = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+        private readonly IConfigurationRoot Configuration;
+        private readonly byte[] encryptionKey;
+        private readonly byte[] iv;
 
-        var builder = new ConfigurationBuilder()
-            .AddJsonFile(configPath, optional: true, reloadOnChange: true);
+        public AppConfig(byte[] encryptionKey, byte[] iv)
+        {
+            this.encryptionKey = encryptionKey;
+            this.iv = iv;
 
-        Configuration = builder.Build();
-    }
+            var configPath = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
 
-    public string Get(string key) => Configuration[key];
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile(configPath, optional: true, reloadOnChange: true);
 
-    public void Set(string key, string value)
-    {
-        var configFile = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            Configuration = builder.Build();
+        }
 
-        if (!File.Exists(configFile))
-            File.WriteAllText(configFile, "{}");
+        public string? Get(string key)
+        {
+            var encryptedValue = Configuration[key];
+            if (string.IsNullOrWhiteSpace(encryptedValue))
+                return null;
 
-        var json = File.ReadAllText(configFile);
-        dynamic config = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
-        config[key] = value;
-        File.WriteAllText(configFile,
-            Newtonsoft.Json.JsonConvert.SerializeObject(config, Newtonsoft.Json.Formatting.Indented));
+            var decryptedValue = Decrypt(encryptedValue, encryptionKey, iv);
+            return decryptedValue;
+        }
 
-        Configuration.Reload();
+        public void Set(string key, string value)
+        {
+            var encryptedValue = Encrypt(value, encryptionKey, iv);
+            var configFile = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+
+            if (!File.Exists(configFile))
+                File.WriteAllText(configFile, "{}");
+
+            var json = File.ReadAllText(configFile);
+            dynamic config = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            config[key] = encryptedValue;
+            File.WriteAllText(configFile,
+                Newtonsoft.Json.JsonConvert.SerializeObject(config, Newtonsoft.Json.Formatting.Indented));
+
+            Configuration.Reload();
+        }
+
+        private string Encrypt(string input, byte[] key, byte[] iv)
+        {
+            using var aesAlg = Aes.Create();
+            aesAlg.Key = key;
+            aesAlg.IV = iv;
+
+            var encryptor = aesAlg.CreateEncryptor(key, iv);
+
+            using var msEncrypt = new MemoryStream();
+            using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+            using (var swEncrypt = new StreamWriter(csEncrypt))
+            {
+                swEncrypt.Write(input);
+            }
+
+            return Convert.ToBase64String(msEncrypt.ToArray());
+        }
+
+        private string Decrypt(string encryptedInput, byte[] key, byte[] iv)
+        {
+            using var aesAlg = Aes.Create();
+            aesAlg.Key = key;
+            aesAlg.IV = iv;
+
+            var decryptor = aesAlg.CreateDecryptor(key, iv);
+
+            using var msDecrypt = new MemoryStream(Convert.FromBase64String(encryptedInput));
+            using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+            using var srDecrypt = new StreamReader(csDecrypt);
+            return srDecrypt.ReadToEnd();
+        }
     }
 }
